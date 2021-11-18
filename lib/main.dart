4b46c1d28731602e28012f31f96be29325134f21
abdelrahman Amer <abdelrahman.amer1999@gmail.com>
2021-11-18 21:36:58 +0200

    almost there

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,16 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdiceno = 1;
+  int rightdiceno = 2;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -26,11 +36,14 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
-                onPressed: (){
-                  print('Left button pressed');
+                onPressed: () {
+                  setState(() {
+                    leftdiceno = Random().nextInt(6)+1;
+                    print('left dice number = $leftdiceno');
+                  });
                 },
                 child: Image(
-                  image: AssetImage('images/dice1.png'),
+                  image: AssetImage('images/dice$leftdiceno.png'),
                 ),
               ),
             ),
@@ -40,11 +53,14 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
-                onPressed: (){
-                  print('Right button pressed');
+                onPressed: () {
+                  setState(() {
+                    rightdiceno = Random().nextInt(6)+1;
+                    print('right dice number = $rightdiceno');
+                  });
                 },
                 child: Image(
-                  image: AssetImage('images/dice1.png'),
+                  image: AssetImage('images/dice$rightdiceno.png'),
                 ),
               ),
             ),
